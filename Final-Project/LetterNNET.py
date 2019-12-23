198d7f783eeb9b575d7d9c0a963ef16e8c2d7f2f
Henry_Puma <pumahenry@live.com>
2019-12-23 04:13:10 -0500

    Added_Project_Files

---
+++ Final-Project/LetterNNET.py
@@ -0,0 +1,21 @@
+class LetterNNET:
+    def __init__(self):
+        self.T_1H = []
+        self.T_2H = []
+        self.T_3H = []
+        self.T_4H = []
+
+        self.T_1L = []
+        self.T_2L = []
+        self.T_3L = []
+        self.T_4L = []
+    def generateHData(self, file_name, dataSize):
+        pass
+    def generateLData(self,file_name, dataSize):
+        pass
+    def sampleTesting(self,sample_data_fname):
+        pass
+    def trainHSet(self, data_fname):
+        pass
+    def trainLSet(self, data_fname):
+        pass
