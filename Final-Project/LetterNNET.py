be5ff04cc36b611948d7c7918052aeacaaac2efc
Hpuma <pumahenry@live.com>
2020-01-02 18:18:47 -0500

    Done_needs_refining_and_comments

--- Final-Project/LetterNNET.py
+++ Final-Project/LetterNNET.py
@@ -91,7 +91,6 @@ class LetterNNET:
                 str_val = self.tuple_to_int(Sm_set[i])
                 self.T_L[i][str_val] += 1
     # Neural Network Tools
-
     # Generates a H or L class list with a randomized pixel as noise
     def generateHList(self, num_noise):
         new_h = list.copy(self.H[randrange(0,3)]) # Making a copy of the H template
@@ -121,12 +120,21 @@ class LetterNNET:
                 new_list = self.generateLList(dataNoise)
                 sampleData.append(0)
             print(new_list,file=current_file)
-        print(sampleData,file=current_file)
-
+        print(sampleData,file=current_file,end="")
+    # Gets the last list from the sample data because it is the only list that tells us which letters were randomly generated
     def getSampleList(self, sampleData):
         sampleList = sampleData[-1]
         sampleData.pop()
         return sampleList
+    def getSampleLetters(self, sampleList):
+        letterData = [0,0]
+        for i in sampleList:
+            if i == 0:
+                letterData[i]+=1
+            elif i == 1:
+                letterData[i]+=1
+        return letterData
+
     # Generates "dataSize" random H arrays with a randomized noise index. ALl the random array get stored in "file_name"
     def generateHData(self, file_name, dataSize, dataNoise):
         current_file = open(file_name,"w")
@@ -144,22 +152,19 @@ class LetterNNET:
     # Takes in an H dataset from a text file and updates the T_H arrays based on the tuple sizes
     def trainHSet(self, data_fname, tuple_size):
         letter_data = self.grabData(data_fname)
-        letter_length = letter_data[0]
-        letter_info = self.getSampleList(letter_data)
+        letter_length = len(letter_data[0])
         self.H_Jm = self.build_JmSet(letter_length,tuple_size)
         for i in letter_data:
-            if(len(i) == letter_length):
-                Sm_set = self.build_SmSet(i,self.H_Jm,tuple_size)
-                self.train_Sm(Sm_set,True)
+            Sm_set = self.build_SmSet(i,self.H_Jm,tuple_size)
+            self.train_Sm(Sm_set,True)
             
         # PRINTING THE self.T_H trained set
         self.print_training_set("H")
     # Takes in an H dataset from a text file and updates the T_H arrays based on the tuple sizes
     def trainLSet(self, data_fname, tuple_size):
         letter_data = self.grabData(data_fname)
-        letter_length 
-        letter_info = self.getSampleList(letter_data)
-        self.L_Jm = self.build_JmSet(len(letter_data[0]),tuple_size)
+        letter_length = len(letter_data[0])
+        self.L_Jm = self.build_JmSet(letter_length,tuple_size)
         for i in letter_data:
             Sm_set = self.build_SmSet(i,self.L_Jm,tuple_size)
             self.train_Sm(Sm_set,False)
@@ -168,18 +173,41 @@ class LetterNNET:
     # CWO: 
     def sampleTesting(self, sample_data_fname,tuple_size):
         sample_data = self.grabData(sample_data_fname)
-        sample_info = self.getSampleList(sample_data)
+        sample_list = self.getSampleList(sample_data)
+        sample_letters = self.getSampleLetters(sample_list)
         H_count = 0
         L_count = 0
         H_sum = 0
         L_sum = 0
+        current_letter = 0
+        correct = 0
         for i in sample_data:
+            correct_letter = sample_list[current_letter] 
             H_sum = self.compute_list_val(i,"H",tuple_size)
             L_sum = self.compute_list_val(i,"L",tuple_size)
+            print("Letter List:",current_letter,"\t",i,end="\t")
+            print("Actual:\t",end="")
+            if correct_letter == 1:
+                print("H",end="\t\t")
+            elif correct_letter == 0:
+                print("L",end="\t\t")
+            print("Guess:",end="\t")
             if H_sum > L_sum:
                 H_count+=1
+                print("H")
+                if correct_letter == 1:
+                    correct+=1
             else:
                 L_count+=1
+                print("L")
+                if correct_letter == 0:
+                    correct+=1
+            current_letter+=1
         # Prints the number of H and L guessed
-        print("H:\t",H_count)
+        print("Actual Count:",end="\t")
+        print("H:\t",sample_letters[1],end="\t")
+        print("L:\t",sample_letters[0])
+        print("Guess Count:",end="\t")
+        print("H:\t",H_count,end="\t")
         print("L:\t",L_count)
+        print("Accuracy:\t",correct/len(sample_data))
